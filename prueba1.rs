88d98a64faeb339d05279bf7d85a86a0c750a470
Txauuuul <saultauste22@gmail.com>
2024-05-03 20:06:04 +0200

    prueba1

---
+++ prueba1.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut name: &str = "Saul";
+
+    println!("Hello, {}!", name);
+
+    name = "Ramon";
+
+    println!("Hello, {}!", name);
+
+    println!("Casi me vuelvo loco, ya que en el original era 'fn print' y me daba error continuamente.");
+}   
