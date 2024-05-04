7922e8dc4629f84e4293f197824b35d399322735
Txauuuul <saultauste22@gmail.com>
2024-05-04 10:54:03 +0200

    actualizacion4

--- prueba5.rs
+++ prueba5.rs
@@ -1,7 +1,7 @@
 fn main() {
     let nums = vec![10, 20, 30, 40];
 
-    for n in nums {
+    for &n in &nums {
         match n {
             30 => println!("thirty"),
             _ => println!("{:?}", n),
