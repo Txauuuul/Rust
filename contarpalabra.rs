85bd8ae344f6e1c25b5dcaef5d8803f56e79e423
Txauuuul <saultauste22@gmail.com>
2024-05-10 12:05:27 +0200

    actualizacion6

--- contarpalabra.rs
+++ contarpalabra.rs
@@ -4,10 +4,10 @@ fn main() {
 
             println!("En la palabra '{}' la primera letra es '{}'.", s, letra);
 
-            println!(
-                "La función chars lo que hace es almacenar los carácteres de la palabra Hola, la función next hace que obtenga el programa 
-            la letra H, ya que si no, como me explicaste el otro día, el valor sería 0, y por último unwrap_or, lo que hace es que aun que no haya 
-            valor en S, no de error de compilacion. Otra opcion sería obviar el unwrap_or y utilizar la funcion match con su respectivo some y none. 
-            Necesitamos hacer esto ya que una función char no puede aparecer en println por si sola, al parecer."
-            );
+        
+            //La función chars lo que hace es almacenar los carácteres de la palabra Hola, la función next hace que obtenga el programa obtenga
+            //la letra H, ya que si no, como me explicaste el otro día, el valor sería 0, y por último unwrap_or, lo que hace es que aun que no haya 
+            //valor en S, no de error de compilacion. Otra opcion sería obviar el unwrap_or y utilizar la funcion match con su respectivo some y none. 
+            //Necesitamos hacer esto ya que una función char no puede aparecer en println por si sola, al parecer."
+
 }
